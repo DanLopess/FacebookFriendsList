c0de20d423705a8a996399f955a6ee97af6e5f74
Daniel <36997776+DanLopess@users.noreply.github.com>
2020-12-26 15:44:52 +0000

    Initial Commit

---
+++ FacebookFriendsList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookFriendsList
+//
+//  Created by Daniel Lopes on 26/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacebookFriendsList/FacebookFriendsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookFriendsListApp.swift
+//  FacebookFriendsList
+//
+//  Created by Daniel Lopes on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FacebookFriendsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
